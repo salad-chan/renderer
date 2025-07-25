8ece99fd0e45d12d5ccdaf501465eb9b569c54d9
Ivan <na>
2025-07-25 14:07:11 -0400

    Bresenhams line implemented Implemented a non safe line drawing function. Further work needed.

--- main.cpp
+++ main.cpp
@@ -8,10 +8,25 @@ constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
 void drawLine(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
+    bool steep = std::abs(ax-bx) < std::abs(ay-by);
+    if (steep) {
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+
+    if (ax>bx) {
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+
     for (float x=ax; x<bx; x++) {
         float t = (x-ax)/static_cast<float>(bx-ax);
         int y = std::round(ay+(by-ay)*t);
-        framebuffer.set(x, y, color);
+        if (steep) {
+            framebuffer.set(y, x, color);
+        } else {
+            framebuffer.set(x, y, color);
+        }
     }
 }
 
